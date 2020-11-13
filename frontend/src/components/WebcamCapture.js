88f7371888a27eb916f6ad03570f1cccb3dfc0c8
Drake Schmidt <dschmid2@svsu.edu>
2020-11-13 11:36:57 -0500

    card for guest pass

--- frontend/src/components/WebcamCapture.js
+++ frontend/src/components/WebcamCapture.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import Webcam from 'react-webcam';
+import { Button, Card, Container, Row, Col } from 'react-bootstrap';
 
 const WebcamCapture = () => {
+  const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: 'user',
+  };
   const webcamRef = React.useRef(null);
   const [imgSrc, setImgSrc] = React.useState(null);
 
@@ -12,9 +18,37 @@ const WebcamCapture = () => {
 
   return (
     <>
-      <Webcam audio={false} ref={webcamRef} screenshotFormat='image/jpeg' />
-      <button onClick={capture}>Capture photo</button>
-      {imgSrc && <img src={imgSrc} />}
+      <Container>
+        <Row>
+          <Col>
+            <Webcam
+              audio={false}
+              height={220}
+              width={380}
+              ref={webcamRef}
+              videoConstraints={videoConstraints}
+              screenshotFormat='image/jpeg'
+            />
+            <Button variant='primary' onClick={capture}>
+              Capture photo
+            </Button>
+          </Col>
+          <Col>
+            <Card style={{ width: '18rem' }} border='secondary'>
+              <Card.Img variant='top' src={imgSrc} />
+              <Card.Body>
+                <Card.Title>Guest Name</Card.Title>
+                <Card.Text>
+                  Guest info: Bob is here to see Bill at 1:30
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Button variant='secondary'>Print Guest Pass</Button>
+          </Col>
+        </Row>
+      </Container>
+
+      {/* {imgSrc && <img src={imgSrc} alt={imgSrc} />} */}
     </>
   );
 };
