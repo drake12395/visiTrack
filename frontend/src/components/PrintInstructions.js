94faf52e48737cb739d505895361e77aacf6f887
Drake Schmidt <drake12395@gmail.com>
2020-12-01 08:57:27 -0500

    simple text component to be toggled at pass print

---
+++ frontend/src/components/PrintInstructions.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const PrintInstructions = () => {
+  return (
+    <div className='HeaderText'>
+      <h1>Your Visitor Pass</h1>
+      <h6>
+        Please print the pass and insert it into the protective case attached to
+        the lanyard that has been provided. Feel free to check out the meeting
+        details. Your host will be with you shortly.
+      </h6>
+    </div>
+  );
+};
+
+export default PrintInstructions;
