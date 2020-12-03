93828a58af03bd3c7ead208cf938baf955639eb6
Drake Schmidt <drake12395@gmail.com>
2020-12-03 14:54:02 -0500

    date/time of meeting

--- backend/models/meetingModel.js
+++ backend/models/meetingModel.js
@@ -2,10 +2,20 @@ import mongoose from 'mongoose';
 
 const meetingSchema = mongoose.Schema(
   {
-    user: {
+    // might need an array here to handle visitor and host, reference lesson 19 at minute 10 for this
+    visitor: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
       ref: 'User',
+    },
+    host: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    meetDayTime: {
+      type: Date,
+      required: true,
     },
     location: {
       type: String,
