6291633a8d7e99c369e86c6c94b278eb1432155a
Drake Schmidt <drake12395@gmail.com>
2020-12-06 07:29:58 -0500

    user actions and reducers

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,13 +3,21 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { meetingListReducer } from './reducers/meetingReducers';
+import { userLoginReducer } from './reducers/userReducers';
 
 const reducer = combineReducers({
   meetingList: meetingListReducer,
+  userLogin: userLoginReducer,
 });
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
+
 // what we want to be loaded when the store loads initially
-const initialState = {};
+const initialState = {
+  userLogin: { userInfo: userInfoFromStorage },
+};
 
 const middleware = [thunk];
 
