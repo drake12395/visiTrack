db6328c094f04cfd343c16d60e396e10fed05e8b
Drake Schmidt <drake12395@gmail.com>
2020-12-06 07:10:34 -0500

    registration and passowrd encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -14,7 +14,7 @@ const authUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
-      isAdmin: user.isAdmin,
+      isHost: user.isHost,
       token: generateToken(user._id),
     });
   } else {
@@ -23,4 +23,55 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+// register a new user
+// POST /api/users   (public)
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already Exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isHost: user.isHost,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid User Data');
+  }
+});
+
+// get user profile
+// GET /api/users/profile    (private)
+const getUserProfile = asyncHandler(async (req, res) => {
+  // get current logged in user
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isHost: user.isHost,
+    });
+  } else {
+    res.status(401);
+    throw new Error('User not found');
+  }
+});
+
+export { authUser, getUserProfile, registerUser };
