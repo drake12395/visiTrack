06cd4ac4505ec839b42fda496cc7846d6b2f552a
Drake Schmidt <drake12395@gmail.com>
2020-12-31 10:30:46 -0500

    reducer and constants for update user/user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -74,4 +74,32 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser };
+// update user profile
+// PUT /api/users/profile    (private)
+const updateUserProfile = asyncHandler(async (req, res) => {
+  // get current logged in user
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isHost: updatedUser.isHost,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('User not found');
+  }
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile };
