10b560e804964b87f9ba74e6eaef25ac03606401
Drake Schmidt <drake12395@gmail.com>
2020-12-31 10:06:36 -0500

    updated instructions

--- frontend/src/components/DemoModeInstructions.js
+++ frontend/src/components/DemoModeInstructions.js
@@ -3,15 +3,23 @@ import React from 'react';
 const DemoModeInstructions = () => {
   return (
     <div>
+      <h4>Demo Mode</h4>
       <h5>
-        Demo mode: This check-in system has two types of user interfaces,
-        visitor and host. Use the following credentials to access the system for
-        each type of user.
+        This check-in system has two types of user interfaces, visitor and host.
+        Use the following credentials to access the system for each type of
+        user. Feel free to create an account, though you will not have a meeting
+        scheduled. Signing in with the demo accounts will show full
+        functionality.
       </h5>
-      <h5>visitor email: jane@example.com </h5>
-      <h5>visitor password: 123456</h5>
-      <h5>host email: host@example.com</h5>
-      <h5>host password: 123456</h5>
+      <p>
+        visitor email: jane@example.com
+        <br />
+        visitor password: 123456
+        <br />
+        host email: host@example.com
+        <br />
+        host password: 123456
+      </p>
     </div>
   );
 };
